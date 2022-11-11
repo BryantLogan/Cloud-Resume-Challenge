5712e4410b841b70eec38f91bf992fb05070834d
BryantLogan <bryant.logan1@gmail.com>
2022-11-11 10:38:16 -0800

    final add_count and get_count functions

--- backend/get_count.py
+++ backend/get_count.py
@@ -2,14 +2,45 @@ import os
 import json
 import boto3
 
-client = boto3.client('dynamodb')
-response = client.describe_table(TableName='cloud-resume-challenge')
-print(response['Table']['ItemCount'])
+def count_hits():
+    client = boto3.client('dynamodb')
+    visits = client.get_item(
+        TableName='crc-dynamo-db',
+        Key={
+            'pk':{
+            'S': 'VisitCounter'}
+            },
+            AttributesToGet=[
+                'Hits'])
+    visit_total = int(visits['Item']['Hits']['N'])
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": json.dumps({
+            "message": "success",
+            "hits": visit_total
+        })
+    }
 
-# import json
-# import boto3
 
-# def lambda_handler(event, context):
-#     client = boto3.client('dynamodb')
-#     response = client.describe_table(TableName='cloud-resume-challenge')
-#     return response['Table']['ItemCount']
+def add_hit():
+    client = boto3.client('dynamodb')    
+    response = client.update_item(
+        TableName='crc-dynamo-db',
+        Key={
+            'pk':{
+                'S': 'VisitCounter'}
+        },
+        UpdateExpression= "SET Hits = Hits + :incr",
+        ExpressionAttributeValues ={
+            ':incr': {'N': '1'}
+            },
+        ReturnValues="UPDATED_NEW",
+    )
+    # print(response['Attributes']['Hits']['N'])
+
+
+count_hits()
+add_hit()
